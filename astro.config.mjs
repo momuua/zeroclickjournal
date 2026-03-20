506ee281fa9a922d9a0e849a97cfa4504a7fcb13
momuua <kingo0330@qq.com>
2026-03-20 23:00:45 +0800

    fix: remove sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://zeroclickjournal.com',
   integrations: [
     mdx(),
-    sitemap(),
   ],
   markdown: {
     syntaxHighlight: 'shiki',
